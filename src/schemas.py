2f4c5a990d0e1b04d19c434e58ef15205d3a0e8f
Akenaide <kevinms1@gmail.com>
2024-02-28 23:29:03 +0100

    #2 create schema for card

---
+++ src/schemas.py
@@ -0,0 +1,18 @@
+import typing
+import strawberry
+
+@strawberry.type
+class Card:
+    set_code: str
+    name: str
+    image64: str
+    id_card: str
+    ability: typing.List[str]
+    rarity: str
+    level: int
+    cost: int
+    power: int
+    soul: int
+    color: str
+    card_type: str
+    special_attribute: typing.List[str]
