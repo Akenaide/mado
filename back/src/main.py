5fed2b2c19b1ddd41576d19c12a351dcb2d9107f
Akenaide <kevinms1@gmail.com>
2024-04-30 22:16:45 +0200

    #17 Add simple set API

--- back/src/main.py
+++ back/src/main.py
@@ -1,11 +1,15 @@
 from contextlib import asynccontextmanager
 import os
 from typing import Union
+import typing
 
+import strawberry
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
 from es_client import create_es_api_key
 from es_client import API_KEY_PATH
+from models import object_set
 
 
 @asynccontextmanager
@@ -17,7 +21,21 @@ async def lifespan(app: FastAPI):
     yield
 
 
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
+
+    sets: typing.List[object_set.Set] = strawberry.field(resolver=object_set.get_sets)
+
+
 app = FastAPI(lifespan=lifespan)
+
+schema = strawberry.Schema(Query)
+graphql_app = GraphQLRouter(schema)
+
+app.include_router(graphql_app, prefix="/graphql")
 
 
 @app.get("/")
--- back/src/models/object_set.py
+++ back/src/models/object_set.py
@@ -1,4 +1,7 @@
+import typing
 import strawberry
+
+from es_client import get_es_client
 
 
 @strawberry.type
@@ -8,3 +11,12 @@ class Set:
     image64: str
     image_url: str
     set_code: str
+
+
+def get_sets() -> typing.List[Set]:
+    SET_INDEX = "search-set"
+    es = get_es_client()
+    resp = es.search(index=SET_INDEX)
+    output = [Set(**hit["_source"]) for hit in resp["hits"]["hits"]]
+
+    return output
