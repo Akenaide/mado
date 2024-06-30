4ef109163d9dada4d8ff512600ff5b37496c0b0b
Kevin MS <kevin@vingtcinq.io>
2024-07-01 00:28:03 +0200

    Add import sets

--- back/src/config.py
+++ back/src/config.py
@@ -7,10 +7,13 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     base_dir: str = os.path.dirname(os.path.abspath(__file__))
     cards_dir: str = "/data/cards"
+    set_file: str = "/data/products.json"
     es_cert_path: str = "/certs/ca/ca.crt"
     es_url: str = "https://es01:9200/"
     elastic_username: str = "elastic"
     elastic_password: str
+    set_index: str = "sets"
+    card_index: str = "cards"
 
 
 @lru_cache
